490496200a8e8025ef71ca51f470c3f41ed871ef
Evan Larkin <evan.larkin@vibes.com>
2011-06-08 09:00:15 -0500

    Deleting all the shortcuts for various programs.

--- functions/fish_prompt.fish
+++
@@ -1,5 +0,0 @@
-function fish_prompt --description "Write out the prompt"
-  printf '%s%s %s%s%s%s%s $' (set_color cyan) (whoami) (set_color yellow) (prompt_pwd) (parse_git_branch) (set_color yellow)
-  set_color normal
-  printf ' '
-end
